b954520729e676df07360cb4f9adfb78ed32b9d0
souravtyson <souravtyson@gmail.com>
2023-05-28 15:47:49 +0530

    3 files modified

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // run `node index.js` in the terminal
+import fetch from 'node-fetch';
 
 console.log(`Hello Node.js v${process.versions.node}!`);
 
@@ -10,3 +11,21 @@ const testCase = {
 
 console.log(key);
 console.log(testCase);
+
+console.log('script start');
+
+let myPromise = new Promise((resolve) => {
+  resolve(123);
+});
+
+setTimeout(() => {
+  console.log('timeout');
+});
+
+fetch('https://my-json-server.typicode.com/typicode/demo/posts')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+myPromise.then((res) => console.log(res));
+
+console.log('script end');
